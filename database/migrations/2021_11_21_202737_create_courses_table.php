d4898be2125fffa6669aa172ab423d11673ef583
Carlos Waldo Rojas <krlosw9@gmail.com>
2021-11-23 16:27:51 -0500

    Corregido falla sintaxis en migracion de courses y posts

--- database/migrations/2021_11_21_202737_create_courses_table.php
+++ database/migrations/2021_11_21_202737_create_courses_table.php
@@ -26,8 +26,8 @@ class CreateCoursesTable extends Migration
 
             $table->timestamps();
 
-            $table->foreign('user_id')->reference('id')->on('users');
-            $table->foreign('category_id')->reference('id')->on('categories');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
