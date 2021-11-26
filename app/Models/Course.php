e98920cda67e31fe29bd5627a998933cba271506
Carlos Waldo Rojas <krlosw9@gmail.com>
2021-11-26 13:22:19 -0500

    Creado el listado de lecciones

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -14,6 +14,11 @@ class Course extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
     public function category()
     {
         return $this->belongTo(Category::class);
