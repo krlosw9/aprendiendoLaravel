ad259ed1e7deb96205f7330839c058a8cf05b547
Carlos Waldo Rojas <krlosw9@gmail.com>
2021-11-26 13:37:36 -0500

    Cursos similares

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -39,6 +39,24 @@
                     </p>
                 </div>
             </div>
+            
+            <div class="grid md:grid-cols-2 gap-4 my-8">
+                @foreach ($course->similar() as $course)
+                    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+                        <a href="{{ route('course', $course->slug) }}">
+                            <img src="{{ $course->image }}" class="rounded-md mb-2 " alt="course-image">
+                            <h2 class="text-lg text-gray-600 truncate uppercase">{{ $course->name }}</h2>
+                            <h3 class="text-md text-gray-500">{{ $course->excerpt }}</h3>
+
+                            <img 
+                                src="{{ $course->user->avatar }}" 
+                                class="rounded-full mt-4 mx-auto h-16 w-16" 
+                                alt="avatar-user">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+
         </div>
     </div>
 
